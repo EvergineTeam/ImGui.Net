13d907cfd4c2572c977690c96115ebd2e4406f43
jcant0n <javiercantonferrero@gmail.com>
2022-06-08 21:07:11 +0200

    Adding powershell generator for every imgui library.

---
+++ Generator/Generate-NuGets-Implot.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+	Evergine bindings NuGet Packages generator script, (c) 2022 Evergine Team
+.DESCRIPTION
+	This script generates NuGet packages for the low-level Vulkan bindings used in Evergine
+	It's meant to have the same behavior when executed locally as when it's executed in a CI pipeline.
+.EXAMPLE
+	<script> -version 2021.11.17.1-local
+.LINK
+	https://evergine.com/
+#>
+
+param (
+	[Parameter(mandatory=$true)][string]$version,
+	[string]$outputFolderBase = "nupkgs",
+	[string]$buildVerbosity = "normal",
+	[string]$buildConfiguration = "Release",
+	[string]$implotBindingsCsprojPath = "Evergine.Bindings.Implot\Evergine.Bindings.Implot.csproj"
+)
+
+# Utility functions
+function LogDebug($line)
+{ Write-Host "##[debug] $line" -Foreground Blue -Background Black 
+}
+
+# Show variables
+LogDebug "############## VARIABLES ##############"
+LogDebug "Version.............: $version"
+LogDebug "Build configuration.: $buildConfiguration"
+LogDebug "Build verbosity.....: $buildVerbosity"
+LogDebug "Output folder.......: $outputFolderBase"
+LogDebug "#######################################"
+
+# Create output folder
+$outputFolder = Join-Path $outputFolderBase $versionWithSuffix
+New-Item -ItemType Directory -Force -Path $outputFolder
+$absoluteOutputFolder = Resolve-Path $outputFolder
+
+# Generate packages
+LogDebug "START packaging process"
+& dotnet pack "$implotBindingsCsprojPath" -v:$buildVerbosity -p:Configuration=$buildConfiguration -p:PackageOutputPath="$absoluteOutputFolder" -p:IncludeSymbols=true -p:Version=$version
+
+LogDebug "END packaging process"
