f67edaf770b35afe94d5f7fb662eab4a9a7e74d3
Javier Carnero <jcarnero@plainconcepts.com>
2022-06-23 16:47:26 +0200

    powershell script to compile all dlls in windows

---
+++ Generator/ps_support.ps1
@@ -0,0 +1,9 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
