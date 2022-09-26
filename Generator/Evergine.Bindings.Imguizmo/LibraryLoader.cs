f5757d4231ee325af857ddd5020917da1e71aeec
jcant0n <javiercantonferrero@gmail.com>
2022-09-26 14:05:57 +0200

    Using Evergine.LibraryLoader ImGui, ImPlot, ImNodes, ImGuizmo.

---
+++ Generator/Evergine.Bindings.Imguizmo/LibraryLoader.cs
@@ -0,0 +1,16 @@
+﻿using Evergine.LibraryLoader;
+
+namespace Evergine.Bindings.Imguizmo
+{
+    internal static class ImguizmoLoader
+    {
+        static ImguizmoLoader()
+        {
+            LibraryLoader.LibraryLoader.Instance
+                .Register(Library.Create("cimguizmo")
+                            .AddConfig(DefaultConfig.Instance)
+                            .SetPlatform(Platform.Windows, "cimguizmo.dll"))
+                .Load();
+        }
+    }
+}
---
+++ Generator/Evergine.Bindings.Imnodes/LibraryLoader.cs
@@ -0,0 +1,16 @@
+﻿using Evergine.LibraryLoader;
+
+namespace Evergine.Bindings.Imnodes
+{
+    internal static class ImnodesLoader
+    {
+        static ImnodesLoader()
+        {
+            LibraryLoader.LibraryLoader.Instance
+                .Register(Library.Create("cimnodes")
+                            .AddConfig(DefaultConfig.Instance)
+                            .SetPlatform(Platform.Windows, "cimnodes.dll"))
+                .Load();
+        }
+    }
+}
