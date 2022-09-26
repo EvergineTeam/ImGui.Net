ddae78d135efbafe26f06a67fbdcf3722f237ad9
jcant0n <javiercantonferrero@gmail.com>
2022-09-26 12:56:59 +0200

    Using Evergine.LibraryLoader on ImGui binding.

---
+++ Generator/Evergine.Bindings.Imgui/LibraryLoader.cs
@@ -0,0 +1,16 @@
+﻿using Evergine.LibraryLoader;
+
+namespace Evergine.Bindings.Imgui
+{
+    internal static class ImGuiLoader
+    {
+        static ImGuiLoader()
+        {
+            LibraryLoader.LibraryLoader.Instance
+                .Register(Library.Create("cimgui")
+                            .AddConfig(DefaultConfig.Instance)
+                            .SetPlatform(Platform.Windows, "cimgui.dll"))
+                .Load();
+        }
+    }
+}
