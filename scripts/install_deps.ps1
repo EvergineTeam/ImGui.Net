b74471dae793af9f117f535613bfa2d4615e9a9e
Alejandro Juan Pérez <tuketet@gmail.com>
2025-04-08 15:00:27 +0200

    rename

--- scripts/install_deps.ps1
+++ scripts/install_deps.ps1
@@ -4,12 +4,12 @@ $tmpFolder = "$PSScriptRoot/../tmp"
 New-Item -ItemType Directory -Path $tmpFolder -Force
 Set-Location $tmpFolder
 
-Invoke-WebRequest -Uri https://github.com/emscripten-core/emsdk/archive/master.zip -OutFile master.zip
+Invoke-WebRequest -Uri https://github.com/emscripten-core/emsdk/archive/master.zip -OutFile emsdk_master.zip
 Expand-Archive -Path master.zip -DestinationPath . -Force
 .\emsdk-master\emsdk update
 .\emsdk-master\emsdk install latest
 .\emsdk-master\emsdk activate latest
-Remove-Item master.zip
+Remove-Item emsdk_master.zip
 
 Invoke-WebRequest -Uri https://github.com/ninja-build/ninja/releases/latest/download/ninja-win.zip -OutFile ninja.zip
 Expand-Archive -Path ninja.zip -DestinationPath . -Force
