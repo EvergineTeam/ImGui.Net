c6dea2c2f4bb5ac577b9f64d1f465cd32535ed33
Alejandro Juan Pérez <tuketet@gmail.com>
2025-05-07 13:58:44 +0200

    WIP script to build native libs in local

--- scripts/build_native.py
+++ scripts/build_native.py
@@ -21,6 +21,12 @@ def rootDir(): return os.path.abspath(os.path.join(scriptsDir(), ".."))
 def nativeLibsDir(): return os.path.join(rootDir(), "NativeLibraries")
 def tmpDir(): return os.path.join(rootDir(), "tmp")
 
+def srcPathForLib(libName):
+    if libName == "cimgui":
+        return os.path.join(nativeLibsDir(), "cimgui")
+    else:
+        return os.path.join(nativeLibsDir(), "extensions", "imgui")
+
 def makeTmpDir():
     tmp_dir = tmpDir()
     if not os.path.exists(tmp_dir):
@@ -43,10 +49,7 @@ def compile_for_wasm(libName="cimgui"):
     subprocess.call([emsdkScriptPath, "install", "latest"])
     subprocess.call([emsdkScriptPath, "activate", "latest"])
     
-    if libName == "cimgui":
-        srcPath = os.path.join(nativeLibsDir(), "cimgui")
-    else:
-        srcPath = os.path.join(nativeLibsDir(), "extensions", "imgui")
+    srcPath = srcPathForLib(libName)
     buildPath = os.path.join(srcPath, "build", "wasm")
     
     emsdk_env = os.path.join(emsdkPath, "emsdk_env.bat" if sys.platform.startswith("win") else "emsdk_env.sh")
@@ -55,6 +58,7 @@ def compile_for_wasm(libName="cimgui"):
     toolchainFile = os.path.join(emsdkPath, "upstream", "emscripten", "cmake", "Modules", "Platform", "Emscripten.cmake")
     crossCompilingEmulator = os.path.join(emsdkPath, "node", "20.18.0_64bit", "bin", "node.exe")
 
+    ninjaExePath_absolute = "C:\\APPS\\ninja\\ninja.exe"
     subprocess.call(["cmake",
         "-S", srcPath,
         "-B", buildPath,
@@ -64,18 +68,49 @@ def compile_for_wasm(libName="cimgui"):
         "-DIMGUI_WCHAR32=ON",
         f"-DCMAKE_BUILD_TYPE={ReleaseMode}",
         "-DIMGUI_FREETYPE=OFF",
-        f'-DCMAKE_CXX_FLAGS="-s USE_FREETYPE=0 -s WASM=1"',
-        f'-DCMAKE_C_FLAGS="-s USE_FREETYPE=0 -s WASM=1"',
-        #f'-DCMAKE_LD_FLAGS="-s USE_FREETYPE=0 -s WASM=1"'
+        # f'-DCMAKE_CXX_FLAGS="-s USE_FREETYPE=0"', # -s WASM=1"',
+        # f'-DCMAKE_C_FLAGS="-s USE_FREETYPE=0"', # -s WASM=1"',
+        # f'-DCMAKE_LD_FLAGS="-sUSE_FREETYPE=0"', # -sWASM=1"'
+        "-GNinja",
+        f"-DCMAKE_MAKE_PROGRAM={ninjaExePath_absolute}"
     ])
     
     subprocess.call(["cmake", "--build", buildPath, "--config", ReleaseMode])
 
-    srcLibPath = os.path.join(buildPath, ReleaseMode, f"{libName}.a")
+    srcLibPath = os.path.join(buildPath, f"{libName}.a")
     dstLibPath = os.path.join(rootDir(), "Generator", libName_to_bindingName[libName], "runtimes", "browser-wasm", "native", f"{libName}.a")
     if os.path.exists(dstLibPath):
         os.remove(dstLibPath)
-    shutil.copy(srcLibPath, dstLibPath)
+    os.makedirs(os.path.dirname(dstLibPath), exist_ok=True)
+    shutil.copy2(srcLibPath, dstLibPath)
+
+def compile_for_win(libName="cimgui"):
+    srcPath = srcPathForLib(libName)
+    buildPath = os.path.join(srcPath, "build", "win-x64",)
+
+    #triplet = "x64-windows-static"
+    #subprocess.call(["vcpkg", "install", "freetype", "--triplet", triplet])
+
+    subprocess.call(["cmake",
+        "-S", srcPath,
+        "-B", buildPath,
+        #f"-DVCPKG_TARGET_TRIPLET={triplet}",
+        "-DIMGUI_WCHAR32=ON",
+        f"-DCMAKE_BUILD_TYPE={ReleaseMode}",
+        "-DIMGUI_FREETYPE=OFF",
+        # f'-DCMAKE_CXX_FLAGS="-s USE_FREETYPE=0"', # -s WASM=1"',
+        # f'-DCMAKE_C_FLAGS="-s USE_FREETYPE=0"', # -s WASM=1"',
+        # f'-DCMAKE_LD_FLAGS="-sUSE_FREETYPE=0"', # -sWASM=1"'
+    ])
+
+    subprocess.call(["cmake", "--build", buildPath, "--config", ReleaseMode])
+    srcLibPath = os.path.join(buildPath, ReleaseMode, f"{libName}.dll")
+    dstLibPath = os.path.join(rootDir(), "Generator", libName_to_bindingName[libName], "runtimes", "win-x64", "native", f"{libName}.dll")
+    if os.path.exists(dstLibPath):
+        os.remove(dstLibPath)
+    os.makedirs(os.path.dirname(dstLibPath), exist_ok=True)
+    shutil.copy2(srcLibPath, dstLibPath)
 
 
 compile_for_wasm()
+compile_for_win("cimgui")
